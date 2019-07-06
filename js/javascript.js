faee2350387e28d1142499deb19b56df4b5ff863
Zhanarbek Osmonaliev <janar.osmonaliev@gmail.com>
2019-07-07 04:25:43 +0900

    Portfolio Update

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+   function fnct(){
+     document.getElementsByClassName('java')[0].setAttribute("style", "box-shadow: 0px 0px 10px #5de1ff;")
+   }
+   function fnctOff(){
+     document.getElementsByClassName('java'[0]).removeAttribute("style")
+   }
+  </script>
